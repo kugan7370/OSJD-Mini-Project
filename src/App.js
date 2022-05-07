3a92cab7626dfd0753fb3e053c9810d78bdfc56a
SVFrank <victorfrank03301997@gmail.com>
2022-05-07 23:51:25 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Franks first projec </code> and save to reload.
+          Edit <code>Franks first project </code> and save to reload.
         </p>
         <a
           className="App-link"
