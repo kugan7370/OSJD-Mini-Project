5a02ff6609b251faed1fc6dc2c80a912c70d3b42
Kugan Priyan <88609808+kugan7370@users.noreply.github.com>
2022-05-09 14:46:03 +0530

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Contact from './Pages/Contact';
+import Service from './Pages/Service';
+import About from './Pages/About';
+import Home from './Pages/Home';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+
+        <Route path="/service" element={<Service />} />
+      </Routes>
+
     </div>
   );
 }
