640ecc6be320857c0385e41b2cd2f46ccfc16543
Thanistas <thanithanis171198@gmail.com>
2022-05-10 14:02:22 +0530

    Modify category

--- src/Pages/SportsCat.jsx
+++ src/Pages/SportsCat.jsx
@@ -5,11 +5,11 @@ function SportsCat() {
       <div>
           <div className='bg-black relative'>
               <img src="/images/cat01.jpg" alt="bgImg" className=' h-[calc(100vh-97.56px)] ' />
-              <div className='absolute top-20 right-0 text-white left-1/2 '>
-                  <div className=' flex flex-col space-y-5'>
+              <div className='absolute top-24 right-0 text-white left-1/2 '>
+                  <div className=' flex flex-col space-y-7'>
                       <span className='text-lg'>- YOU WANT IT YOU GOT IT</span>
-                      <span className='text-7xl font-bold'>Buy Your</span>
-                      <span className='text-7xl font-bold '>jersy right now</span>
+                      <span className='text-7xl font-bold'>Buy Your Jersy</span>
+                      <span className='text-7xl font-bold '>right now</span>
                       <div>
                           <div className='bg-[#ff0c3c] rounded-3xl w-24 p-2 mt-10'>BUY NOW</div>
                           {/* #8b2a26 */}
