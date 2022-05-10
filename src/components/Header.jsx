b6764d57d9c719d042df6232f51ceffa7f4e3e6e
Thanistas <thanithanis171198@gmail.com>
2022-05-10 13:24:51 +0530

    Add category page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
 
             <ul className=' flex space-x-10 m-auto'>
                 <Link to="/"><li className='uppercase font-bold text-white'>sports</li></Link>
-                <Link to='/about'> <li className='uppercase font-bold text-white'>about us</li> </Link>
+                <Link to='/Catgorary'> <li className='uppercase font-bold text-white'>about us</li> </Link>
                 <Link to='/service'><li className='uppercase font-bold text-white'>services</li> </Link>
                 <Link to='/contact'><li className='uppercase font-bold text-white'>contacts</li></Link>
 
